4d1763f7065915763f874c350ab12cf525239bda
nickie kane <kameo712@gmail.com>
2019-12-08 22:29:42 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(says)
+  
+  if says.length > 0 
+    puts says
+  else
+    puts "Squawk!"
+  end
+   return says
+  
+end
+
+parrot()
